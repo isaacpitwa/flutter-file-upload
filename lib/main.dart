a20b908c9e7da5716960e0a1777f4c4fa29f6b76
isaacpitwa <isaacpitwa256@gmail.com>
2022-01-30 13:56:57 +0300

    Feat: preview file to upload

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,18 @@ class _MyHomePageState extends State<MyHomePage>
 
     loadingController.forward();
   }
-  
+
+
+  @override
+  void initState() {
+    loadingController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 10),
+    )..addListener(() { setState(() {}); });
+    
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -98,7 +109,7 @@ class _MyHomePageState extends State<MyHomePage>
                 child: DottedBorder(
                   borderType: BorderType.RRect,
                   radius: const Radius.circular(10),
-                  dashPattern: [10, 4],
+                  dashPattern: const [10, 4],
                   strokeCap: StrokeCap.round,
                   color: Colors.blue.shade400,
                   child: Container(
@@ -120,6 +131,77 @@ class _MyHomePageState extends State<MyHomePage>
                 )
               ),
             ),
+            _platformFile != null
+              ? Container(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Selected File', 
+                      style: TextStyle(color: Colors.grey.shade400, fontSize: 15, ),),
+                    const SizedBox(height: 10,),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.shade200,
+                            offset: const Offset(0, 1),
+                            blurRadius: 3,
+                            spreadRadius: 2,
+                          )
+                        ]
+                      ),
+                      child: Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Image.file(_file!, width: 70,)
+                          ),
+                          const SizedBox(width: 10,),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(_platformFile!.name, 
+                                  style: const TextStyle(fontSize: 13, color: Colors.black),),
+                                const SizedBox(height: 5,),
+                                Text('${(_platformFile!.size / 1024).ceil()} KB', 
+                                  style: TextStyle(fontSize: 13, color: Colors.grey.shade500),
+                                ),
+                                const SizedBox(height: 5,),
+                                Container(
+                                  height: 5,
+                                  clipBehavior: Clip.hardEdge,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    color: Colors.blue.shade50,
+                                  ),
+                                  child: LinearProgressIndicator(
+                                    value: loadingController.value,
+                                  )
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(width: 10,),
+                        ],
+                      )
+                    ),
+                    const SizedBox(height: 20,),
+                    // MaterialButton(
+                    //   minWidth: double.infinity,
+                    //   height: 45,
+                    //   onPressed: () {},
+                    //   color: Colors.black,
+                    //   child: Text('Upload', style: TextStyle(color: Colors.white),),
+                    // )
+                  ],
+                ))
+              : Container(),
+              const SizedBox(height: 150,),
         ],
       ),
     ));
